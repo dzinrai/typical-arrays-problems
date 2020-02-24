213ca7232c9dc21808946b2e84240c0e711c0ed8
dzinrai <dzinrai@mail.ru>
2020-02-24 13:21:52 +0300

    feat: implement min,max,avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 exports.min = function min (array) {
-  return 0;
+  if( !array || array.length === 0 ) return 0;
+  array.sort( (a,b) => { return a - b; } );
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if( !array || array.length === 0 ) return 0;
+  array.sort( (a,b) => { return b - a; } );
+  return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if( !array || array.length === 0 ) return 0;
+  return array.reduce( (a,b) => { return a + b; } )/array.length;
 }
